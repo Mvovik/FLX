bd51e2d0b6976afafd2dfa248009fe6df6682ffb
Mykhailo Svystun <msvystun@ukr.net>
2019-02-04 13:13:28 +0200

    homework 6

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,3 +1,24 @@
-// Your code goes here
+function quadraticEquation() {
 
-console.log('dsds');
+    var a = prompt('Please input' + ' "a" ' + 'value', '');
+    var b = prompt('Please input' + ' "b" ' + 'value', '');
+    var c = prompt('Please input' + ' "c" ' + 'value', '');
+
+    if (isNaN(a) || a === '' || isNaN(b) || b === '' || isNaN(c) || c === '') {
+        alert('Invalid input data');
+    } else {
+        var d = (b * b) - 4 * a * c;
+        if (d === 0) {
+            var x = -b / 2 * a;
+            alert('x = ' + x);
+        } else if (d < 0) {
+            alert('no solution')
+        } else {
+            var x1 = -b / (2 * a) - Math.sqrt(d) / (2 * a);
+            var x2 = -b / (2 * a) + Math.sqrt(d) / (2 * a);
+            alert('x1 = ' + x1 + ' and ' + 'x2 = ' + x2)
+        }
+    }
+}
+
+quadraticEquation()
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+function discountCalculator() {
+    var price = prompt('Input amount of money ', '');
+    var discount = prompt('Input the discount  ', '');
+    var discountPrice = price - ((price / 100) * discount);
+    var savedMoney = (price / 100) * discount;
+
+    if (price < 0 || price > 9999999 || price === '' || isNaN(price) ||
+        discount < 0 || discount > 99 || discount === '' || isNaN(discount)) {
+        alert('Invalid input data');
+    } else {
+        alert(
+            ' Price without discount: ' + price +
+            '\r\n Discount: ' + discount + '%' +
+            '\r\n Price with discount: ' + discountPrice.toFixed(2) * 1 +
+            '\r\n Saved: ' + savedMoney.toFixed(2) * 1
+        );
+    }
+}
+
+discountCalculator()
