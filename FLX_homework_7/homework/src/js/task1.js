e45837634e3d53a6fa1f576a74b9729c346419ca
Mykhailo Svystun <msvystun@ukr.net>
2019-02-06 00:05:24 +0200

    task 1 done

---
+++ FLX_homework_7/homework/src/js/task1.js
@@ -0,0 +1,40 @@
+let login = prompt("Enter your login", "");
+let date = new Date().getHours();
+
+if (login) {
+  if (login === "User" || login === "Admin") {
+    passwordChekout();
+  } else if (login.length < 4) {
+    alert("I don't know any users having name length less than 4 symbols")
+  } else {
+    alert("I don’t know you")
+  }
+} else {
+  alert("Canceled");
+}
+
+function passwordChekout() {
+  let password = prompt("Enter your password", "");
+
+  if (password) {
+    if (login === "User" && password === 'UserPass') {
+      if (date < 20) {
+        alert("Good day, dear " + login + "!")
+      } else {
+        alert("Good evening, dear " + login + "!")
+      }
+
+    } else if (login === "Admin" && password === 'RootPass') {
+      if (date < 20) {
+        alert("Good day, dear " + login + "!")
+      } else {
+        alert("Good evening, dear " + login + "!")
+      }
+
+    } else {
+      alert("Wrong password")
+    }
+  } else {
+    alert("Canceled");
+  }
+}
